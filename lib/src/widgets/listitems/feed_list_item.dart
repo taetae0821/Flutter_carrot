20ce4489392a1d052461b0b82c2498d762a2dc63
taeyu <s2530@e-mirim.hs.kr>
2026-06-19 21:49:54 +0900

    Listcontent 추가

---
+++ lib/src/widgets/listitems/feed_list_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+const double _imageSize = 110;
+
+class FeedListItem extends StatelessWidget{
+  const FeedListItem({super.key});
+
+  
+
+  @override
+  Widget build(BuildContext context){
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(10.0),
+            child: Image.network("https://example.com/image.jpg",width: _imageSize, height: _imageSize, fit: BoxFit.cover,),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('판매할 물건의 제목',overflow: TextOverflow.ellipsis,style: TextStyle(fontSize: 16),),
+                  Row(children: [
+                    Text('동네이름',style: TextStyle(color: Colors.grey),),
+                    Text('N분전',style: TextStyle(color: Colors.grey),),
+                  ],),
+                  Text('물품 가격',style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                ],
+               ),
+              ),
+              ),
+              IconButton(onPressed: (){}, icon: const Icon(Icons.more_vert,color: Colors.grey,size:16,),),
+        ],
+      ),
+      ),
+    );
+  }
+}
